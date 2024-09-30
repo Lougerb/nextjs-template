d8ac6cbf92fef82bca7160dc78d910c367f5c1f2
Lougerb <glouien@gmail.com>
2024-09-30 15:15:25 +0800

    updated env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { config } from 'dotenv';
-config({ path: '.env.development.local' }); // or .env
+config({ path: '.env' }); // or .env
 export const db = drizzle(sql);
