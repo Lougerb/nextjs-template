06c595733a20dbfb701f277a5c340d51c14aca3b
Lougerb <glouien@gmail.com>
2025-06-10 13:36:41 +0800

    removed unnecessary code

--- src/db/user/queries/select.ts
+++ src/db/user/queries/select.ts
@@ -13,8 +13,7 @@ export async function getAllUsers(){
 export async function getUserById(id: SelectUser['id']): Promise<
   Array<{
     id: number;
-    name: string;
-    age: number;
+    username: string;
     email: string;
   }>
 > {
@@ -22,47 +21,3 @@ export async function getUserById(id: SelectUser['id']): Promise<
 }
 
 
-export async function getUsersWithPostsCount(
-  page = 1,
-  pageSize = 5,
-): Promise<
-  Array<{
-    postsCount: number;
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }>
-> {
-  return db
-    .select({
-      ...getTableColumns(usersTable),
-      postsCount: count(postsTable.id),
-    })
-    .from(usersTable)
-    .leftJoin(postsTable, eq(usersTable.id, postsTable.userId))
-    .groupBy(usersTable.id)
-    .orderBy(asc(usersTable.id))
-    .limit(pageSize)
-    .offset((page - 1) * pageSize);
-}
-export async function getPostsForLast24Hours(
-  page = 1,
-  pageSize = 5,
-): Promise<
-  Array<{
-    id: number;
-    title: string;
-  }>
-> {
-  return db
-    .select({
-      id: postsTable.id,
-      title: postsTable.title,
-    })
-    .from(postsTable)
-    .where(between(postsTable.createdAt, sql`now() - interval '1 day'`, sql`now()`))
-    .orderBy(asc(postsTable.title), asc(postsTable.id))
-    .limit(pageSize)
-    .offset((page - 1) * pageSize);
-}
